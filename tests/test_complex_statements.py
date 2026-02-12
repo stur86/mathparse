be181b81faf1675761ef9456e710eb139c63993d
Gunther Cox <gunthercx@gmail.com>
2026-02-12 07:35:46 -0500

    Fix tests

--- tests/test_complex_statements.py
+++ tests/test_complex_statements.py
@@ -70,11 +70,11 @@ class ComplexStatementsTestCase(TestCase):
         Test using the unicode π symbol as a constant.
         """
         result = mathparse.parse('2 * π')
-        self.assertAlmostEqual(result, 2 * 3.141693, places=5)
+        self.assertAlmostEqual(result, 2 * 3.141593, places=5)
 
     def test_unicode_pi_in_expression(self):
         """
         Test using the unicode π symbol in a complex expression.
         """
         result = mathparse.parse('π + 1')
-        self.assertAlmostEqual(result, 3.141693 + 1, places=5)
+        self.assertAlmostEqual(result, 3.141593 + 1, places=5)
